260ebd4a2b9bbe71dc11264e4baedd260e392e1f
IversonMamangun <177076443+IversonMamangun@users.noreply.github.com>
2026-06-06 09:58:44 +0800

    Refactor Landing page layout, add Solutions Index route, and create new Solutions/Index.vue component

--- resources/js/pages/Landing.vue
+++ resources/js/pages/Landing.vue
@@ -13,11 +13,11 @@ import NavBar from '@/components/NavBar.vue';
 <template>
     <Head title="Landing Page">
     </Head>
-      <section class="relative px-6 lg:px-20 py-20 bg-gradient-to-br from-emerald-900 via-slate-950 to-cyan-950">
-
+   <section class="relative px-6 lg:px-20 py-20 bg-gradient-to-br from-emerald-900 via-slate-950 to-cyan-950">
    <NavBar />
-   <HomeSection />
+   <HomeSection />I
    </section>
+
    <DigitalSolutionSection />
    <section
         class="px-6 lg:px-20 py-24 bg-gradient-to-b from-slate-900 to-slate-950"
